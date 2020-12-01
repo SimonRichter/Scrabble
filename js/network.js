8ffcb26866e18250c6d52e5d29e0c09a3bf63182
tripleWdotcom <mauricio.espinoza@gmail.com>
2020-12-01 15:18:27 +0100

    Create network.js

---
+++ js/network.js
@@ -0,0 +1,18 @@
+import Store from 'https://network-lite.nodehill.com/store';
+import Board from "./Board.js";
+
+export default class Network {
+
+  constructor() {
+    this.idunno = this.board.putTiles;
+
+
+    this.renderStart();
+    this.addEventListeners();
+    this.board.render();
+  }
+
+
+
+
+}
