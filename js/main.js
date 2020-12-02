b0ae5ed165c3aafcfb753087ebc5dd8dd4c5f0f0
Denise <denisebradley92@hotmail.com>
2020-12-02 19:52:29 +0100

    created initial startpage stuff

--- js/main.js
+++ js/main.js
@@ -1,2 +1,4 @@
+import StartPage from './startpage';
 import Game from './Game.js';
+//new StartPage();
 new Game().start();
---
+++ js/startpage.js
@@ -0,0 +1,20 @@
+export default class StartPage {
+
+  constructor() {
+    this.render();
+  }
+
+  render() {
+    $('body').append(`<header class="startpage">
+    <h1>SCRABBLE</h1>
+    <button class="newgame">NYTT SPEL</button>
+    <button class="joingame">GÅ MED I BEFINTLIGT SPEL</button>
+    </header>`);
+
+    //$('body').css('background-color', 'blue !important');
+
+
+  }
+
+
+}
