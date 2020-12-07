2e00ea1d9b4500dddc14ab9087a307caec1d9fe1
Hugo Granath <72066319+mrHugolo@users.noreply.github.com>
2020-12-07 13:11:04 +0100

    tests from friday
    Co-Authored-By: tripleWdotcom <67017215+tripleWdotcom@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
-import Game from './game.js'
-import Network from './Network.js';
-import StartPage from './startpage.js';
-let n = new Network();
-new StartPage();
-let start = 0;
-setTimeout(() => {
-  if (n.store.playerNames.length > 1) { start = 1; }
-  console.log('One minute has passed');
-  if (start === 1) { $('.startpage').remove(); new Game().start(); }
-}, 30000);
+import StartPage from './startpage.js'; new StartPage();
+
+
 
 
